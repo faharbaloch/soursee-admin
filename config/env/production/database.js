5630ddb51b3ea59b21d421f817f659800d9fde45
faharbaloch <faharbaloch22@gmail.com>
2024-09-15 04:44:17 +0500

    all changes has been made

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
     connection: {
-      client: 'mysql2',
+      client: 'postgres',
       connection: {
-        host: env('MYSQLHOST', '127.0.0.1'),
-        port: env.int('MYSQLPORT', 3306),
-        database: env('MYSQLDATABASE', 'strapi'),
-        user: env('MYSQLUSER', 'strapi'),
-        password: env('MYSQLPASSWORD', 'strapi'),
+        host: env('PGHOST', '127.0.0.1'),
+        port: env.int('PGPORT', 3306),
+        database: env('PGDATABASE', 'strapi'),
+        user: env('PGUSER', 'strapi'),
+        password: env('PGPASSWORD', 'strapi'),
        ssl: env.bool(true),
       },
-      debug: false,
+      pool: {min: 0}
     },
   });
